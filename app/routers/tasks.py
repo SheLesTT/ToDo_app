996e79bbeaff9ff0d97742f2c5f3f2629234225d
SheLesTT <lovsevdenis3513@gmail.com>
2023-08-09 18:00:31 +0300

    delete, put, create operations for tasks

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+
 from app.database import get_db
 from sqlalchemy.orm import Session
 from app.models import Task
@@ -9,14 +10,51 @@ router = APIRouter(
 )
 
 posts = [{"id":"1","name":"aa", "content": "Hello World"},{"id":"2","name":"bb", "content": "Hello World"}]
-@router.get("/")
-async def get_posts():
-    return posts[0]
+@router.get("/", response_model=list[schemas.Task])
+async def get_posts(db: Session = Depends(get_db)):
+    tasks = db.query(Task).all()
+    return tasks
 
 @router.get("/{id}", response_model=schemas.Task)
 async def get_post(id:int, db: Session = Depends(get_db)):
     task = db.query(Task).filter(Task.id == id).first()
     if task is None:
         raise HTTPException(status_code=404, detail="User not found")
-
     return task
+
+
+@router.post("/", response_model=schemas.Task)
+async def create_post(post: schemas.TaskCreate, db: Session = Depends(get_db)):
+
+    new_task = Task(name=post.name, content=post.content, )
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return new_task
+
+@router.put("/{id}")
+async def update_content(id:int, task_update :schemas.TaskUpdateContent, db:Session = Depends(get_db)):
+    task = db.query(Task).filter(Task.id == id).first()
+
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    if task_update.content:
+        task.content = task_update.content
+    if task_update.name is not None:
+        task.name = task_update.name
+    if task_update.is_done is not None:
+        task.is_done = task_update.is_done
+
+    db.commit()
+    db.refresh(task)
+    return task
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id:int, db:Session = Depends(get_db)):
+    task = db.query(Task).filter(Task.id == id).first()
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    db.delete(task)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,25 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Task(BaseModel):
     id: int
     name: str
-    content: str
+    content: Optional[str] =None
     is_done: bool
+
+    class Config:
+        orm_mode = True
+
+class TaskCreate(BaseModel):
+    name: str
+    content: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+class TaskUpdateContent(BaseModel):
+    name: Optional[str] = None
+    content: Optional[str] = None
+    is_done: Optional[bool] = None
 
     class Config:
         orm_mode = True
