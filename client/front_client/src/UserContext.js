a55db8954cf3f2c10cf7805dad28e86fd4d45bc0
SheLesTT <lovsevdenis3513@gmail.com>
2023-08-18 11:25:12 +0300

    add user context

---
+++ client/front_client/src/UserContext.js
@@ -0,0 +1,34 @@
+import {createContext} from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = (props) => {
+    const [token, setToken] = useState(localStorage.getItem("userToken"));
+
+    useEfefect(() => {
+        const fetchUser = async () => {
+            const requestOptions = {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+
+            const response = await fetch("http://127.0.0.1:8000/api/login", requestOptions);
+
+            if (!response.ok){
+                setToken(null)
+
+            }
+            localStorage.setItem("userToken", token)
+
+        }
+        fetchUser();
+    }, [token]);
+}
+return (<UserContext.Provider value ={[token, setToken]}>
+    {props.children}
+    </UserContext.Provider>
+)
+
